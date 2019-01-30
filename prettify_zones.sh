66fe5d20c71dfb69a5da0db35a4b9938e67faead
Marco Davids <marco.davids@sidn.nl>
2019-01-30 13:19:36 +0000

    improved prettify

--- prettify_zones.sh
+++ prettify_zones.sh
@@ -9,8 +9,24 @@ cd $DIR
 #
 for a in $(ls *.nl)
 do
-	named-compilezone -i "none" -s "relative" -f text -F text -o $a.txt $a $a
+	# Prepocess the problematic one
+	if [[ $a == *"apexcname.wb.sidnlabs.nl"* ]]; then
+  		#echo "preprocessing $a"
+  		cp $a $a.prep
+  		sed -i "s/\tCNAME\t/\tMB\t/" ./$a.prep
+  		sed -i "s/\ CNAME\ /\ MB\ /" ./$a.prep
+  		named-compilezone -i "none" -s "relative" -f text -F text -o $a.txt $a $a.prep
+	else
+  		named-compilezone -i "none" -s "relative" -f text -F text -o $a.txt $a $a
+	fi
+
+	# Completing the problematic one
+	if [[ $a == *"apexcname.wb.sidnlabs.nl"* ]]; then
+  		#echo "finisching $a"
+  		sed -i "s/\tMB\t/\tCNAME\t/" ./$a.txt
+  		sed -i "s/\ MB\ /\ CNAME\ /" ./$a.txt
+  		rm $a.prep
+	fi
+
 	mv $a.txt $a	
 done
-
-
