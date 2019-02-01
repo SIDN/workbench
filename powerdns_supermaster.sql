9cea4b4cf653d9894bc28eb84bf1e0d3a1ab9707
Marco Davids <marco.davids@sidn.nl>
2019-02-01 19:05:53 +0000

    fixes/improvements powerdns superslaving

--- powerdns_supermaster.sql
+++ powerdns_supermaster.sql
@@ -3,4 +3,5 @@
  DELETE FROM supermasters;
  VACUUM;
  INSERT INTO supermasters values ('2a00:d78:0:712:94:198:159:39', 'bind9.sidnlabs.nl', 'workbench');
+ INSERT INTO supermasters values ('94.198.159.39', 'bind9.sidnlabs.nl', 'workbench');
   
