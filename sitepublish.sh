db5c85994adce10b1169341db9ee6947fb9d3a9e
Marco Davids <marco.davids@sidn.nl>
2019-02-03 21:08:14 +0000

    -

--- sitepublish.sh
+++ sitepublish.sh
@@ -8,7 +8,7 @@ if [[ $EUID -ne 0 ]]; then
   echo "ERROR: You must be a root user. Quitting." 2>&1
 exit 1
 else
-  echo "Publish sequence started:"
+  echo "- Publish sequence started:"
 fi
 
 check_rcode() {
@@ -21,23 +21,23 @@ check_rcode() {
     fi
 }
 
-#echo "Cleanup *~"
+#echo "- Cleanup *~"
 #find . -type f -name "*~" | xargs rm
 #check_rcode
 
-echo "Remove the old version"
+echo "- Remove the old version"
 rm -rf /var/www/html/*
 check_rcode
 rm -rf /var/cache/lighttpd/compress/*
 check_rcode
-echo "Install new version"
+echo "- Install new version"
 cp -a site/* /var/www/html/
 check_rcode
 # Needed for the Directory Listing pages:
 ln -s /var/www/html/assets/favicon.ico /var/www/html/
 check_rcode
 
-echo "Refresh the unsigend and signed zones (types.html refers to them)"
+echo "- Refresh the unsigend and signed zones (types.html refers to them)"
 mkdir /var/www/html/zones
 mkdir /var/www/html/delegationzones
 mkdir /var/www/html/typeszones
@@ -51,3 +51,4 @@ for a in $(ls /var/dns-workbench/zones/*bad-dnssec.wb.sidnlabs.nl | awk -F\/ '{p
 #ln -s /var/dns-workbench/zones/types.wb.sidnlabs.nl /var/www/html/types.wb.sidnlabs.nl.txt
 
 echo "- All done!"
+echo "(if it doesn't work; did you do ./create_site.sh first ???)"
